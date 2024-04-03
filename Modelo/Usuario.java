3f4258c750542553741bf89d50f3da24e921e152
Luis <lfkv69@gmail.com>
2024-04-03 04:32:48 -0600

    Creación clase Administrador y prototipo de la GUI del login

--- Modelo/Usuario.java
+++ Modelo/Usuario.java
@@ -1,5 +1,33 @@
 package Modelo;
 
 public class Usuario {
-    
+    private String nombre;
+    private int id;
+    private String rol;
+    private String turno;
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+    public void setTurno(String turno) {
+        this.turno = turno;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public int getId() {
+        return id;
+    }
+    public String getRol() {
+        return rol;
+    }
+    public String getTurno() {
+        return turno;
+    }
 }
