54c776b081a0597c6cbe90beb183d1efe31d9230
Luis <lfkv69@gmail.com>
2024-04-03 17:12:42 -0600

    GUI login final, sin funcionalidad

--- Vista/Login.java
+++ Vista/Login.java
@@ -1,3 +1,6 @@
+
+import java.awt.Color;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
@@ -8,7 +11,8 @@
  * @author kumul
  */
 public class Login extends javax.swing.JFrame {
-
+    int xMouse, yMouse;
+    
     /**
      * Creates new form Login
      */
@@ -27,6 +31,8 @@ public class Login extends javax.swing.JFrame {
 
         bg = new javax.swing.JPanel();
         titleBar = new javax.swing.JPanel();
+        closeButton = new javax.swing.JPanel();
+        closeTxt = new javax.swing.JLabel();
         content = new javax.swing.JPanel();
         text1 = new javax.swing.JLabel();
         userText = new javax.swing.JLabel();
@@ -39,42 +45,86 @@ public class Login extends javax.swing.JFrame {
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setLocationByPlatform(true);
         setUndecorated(true);
-        setPreferredSize(new java.awt.Dimension(408, 499));
         setResizable(false);
 
         bg.setBackground(new java.awt.Color(255, 255, 255));
 
-        titleBar.setBackground(new java.awt.Color(204, 255, 204));
+        titleBar.setBackground(new java.awt.Color(255, 255, 255));
+        titleBar.addMouseMotionListener(new java.awt.event.MouseMotionAdapter() {
+            public void mouseDragged(java.awt.event.MouseEvent evt) {
+                titleBarMouseDragged(evt);
+            }
+        });
+        titleBar.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mousePressed(java.awt.event.MouseEvent evt) {
+                titleBarMousePressed(evt);
+            }
+        });
+
+        closeButton.setBackground(new java.awt.Color(255, 255, 255));
+        closeButton.setPreferredSize(new java.awt.Dimension(35, 30));
+
+        closeTxt.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        closeTxt.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        closeTxt.setText("X");
+        closeTxt.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                closeTxtMouseClicked(evt);
+            }
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                closeTxtMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                closeTxtMouseExited(evt);
+            }
+        });
+
+        javax.swing.GroupLayout closeButtonLayout = new javax.swing.GroupLayout(closeButton);
+        closeButton.setLayout(closeButtonLayout);
+        closeButtonLayout.setHorizontalGroup(
+            closeButtonLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, closeButtonLayout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addComponent(closeTxt, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE))
+        );
+        closeButtonLayout.setVerticalGroup(
+            closeButtonLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(closeTxt, javax.swing.GroupLayout.DEFAULT_SIZE, 30, Short.MAX_VALUE)
+        );
 
         javax.swing.GroupLayout titleBarLayout = new javax.swing.GroupLayout(titleBar);
         titleBar.setLayout(titleBarLayout);
         titleBarLayout.setHorizontalGroup(
             titleBarLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 0, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, titleBarLayout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addComponent(closeButton, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
         titleBarLayout.setVerticalGroup(
             titleBarLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 30, Short.MAX_VALUE)
+            .addGroup(titleBarLayout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addComponent(closeButton, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
 
-        content.setBackground(new java.awt.Color(204, 255, 255));
+        content.setBackground(new java.awt.Color(255, 255, 255));
 
-        text1.setFont(new java.awt.Font("Segoe UI Black", 0, 14)); // NOI18N
+        text1.setFont(new java.awt.Font("Segoe UI Black", 0, 24)); // NOI18N
         text1.setText("INICIAR SESIÓN");
 
-        userText.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        userText.setFont(new java.awt.Font("Segoe UI", 0, 16)); // NOI18N
         userText.setText("Usuario");
 
-        userIn.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        userIn.setFont(new java.awt.Font("Segoe UI", 0, 16)); // NOI18N
         userIn.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 userInActionPerformed(evt);
             }
         });
 
-        passwordIn.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        passwordIn.setFont(new java.awt.Font("Segoe UI", 0, 16)); // NOI18N
 
-        passwordText.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        passwordText.setFont(new java.awt.Font("Segoe UI", 0, 16)); // NOI18N
         passwordText.setText("Contraseña");
 
         loginButton.setBackground(new java.awt.Color(61, 100, 218));
@@ -88,7 +138,9 @@ public class Login extends javax.swing.JFrame {
             }
         });
 
+        logoImg.setFont(new java.awt.Font("Segoe UI", 1, 36)); // NOI18N
         logoImg.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        logoImg.setText("LOGO");
 
         javax.swing.GroupLayout contentLayout = new javax.swing.GroupLayout(content);
         content.setLayout(contentLayout);
@@ -113,17 +165,17 @@ public class Login extends javax.swing.JFrame {
                 .addComponent(logoImg, javax.swing.GroupLayout.DEFAULT_SIZE, 90, Short.MAX_VALUE)
                 .addGap(18, 18, 18)
                 .addComponent(text1, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGap(22, 22, 22)
                 .addComponent(userText, javax.swing.GroupLayout.PREFERRED_SIZE, 45, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(userIn, javax.swing.GroupLayout.PREFERRED_SIZE, 45, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(28, 28, 28)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(passwordText, javax.swing.GroupLayout.PREFERRED_SIZE, 45, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(passwordIn, javax.swing.GroupLayout.PREFERRED_SIZE, 45, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGap(33, 33, 33)
                 .addComponent(loginButton, javax.swing.GroupLayout.PREFERRED_SIZE, 43, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(56, 56, 56))
+                .addGap(35, 35, 35))
         );
 
         javax.swing.GroupLayout bgLayout = new javax.swing.GroupLayout(bg);
@@ -163,6 +215,30 @@ public class Login extends javax.swing.JFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_userInActionPerformed
 
+    private void titleBarMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_titleBarMousePressed
+        xMouse = evt.getX();
+        yMouse = evt.getY();
+    }//GEN-LAST:event_titleBarMousePressed
+
+    private void titleBarMouseDragged(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_titleBarMouseDragged
+        int x=evt.getXOnScreen(), y=evt.getYOnScreen();
+        this.setLocation(x-xMouse, y-yMouse);
+    }//GEN-LAST:event_titleBarMouseDragged
+
+    private void closeTxtMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_closeTxtMouseClicked
+        System.exit(0);
+    }//GEN-LAST:event_closeTxtMouseClicked
+
+    private void closeTxtMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_closeTxtMouseEntered
+        closeButton.setBackground(Color.red);
+        closeTxt.setForeground(Color.white);
+    }//GEN-LAST:event_closeTxtMouseEntered
+
+    private void closeTxtMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_closeTxtMouseExited
+        closeButton.setBackground(Color.white);
+        closeTxt.setForeground(Color.black);
+    }//GEN-LAST:event_closeTxtMouseExited
+    
     /**
      * @param args the command line arguments
      */
@@ -200,6 +276,8 @@ public class Login extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel bg;
+    private javax.swing.JPanel closeButton;
+    private javax.swing.JLabel closeTxt;
     private javax.swing.JPanel content;
     private javax.swing.JButton loginButton;
     private javax.swing.JLabel logoImg;
