8875eba9b26c77e4d741dfb6275964a4c7afe275
Luis <lfkv69@gmail.com>
2024-04-05 03:06:24 -0600

    Eliminación de la clase Administrador, se agrega el atributo lvlPriv (nivel de privilegio) a la clase Usuario

--- Modelo/Administrador.java
+++
@@ -1,5 +0,0 @@
-package Modelo;
-
-public class Administrador extends Usuario{
-    
-}
--- Modelo/Usuario.java
+++ Modelo/Usuario.java
@@ -3,14 +3,18 @@ package Modelo;
 public class Usuario {
     private int id;
     private String nombre;
+    private int privilegio;
     private String rol;
     private String turno;
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
     public void setId(int id) {
         this.id = id;
+    }
+    public void setPrivilegio(int privilegio) {
+        this.privilegio = privilegio;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
     }
     public void setRol(String rol) {
         this.rol = rol;
@@ -18,11 +22,14 @@ public class Usuario {
     public void setTurno(String turno) {
         this.turno = turno;
     }
-    public String getNombre() {
-        return nombre;
-    }
     public int getId() {
         return id;
+    }
+    public int getPrivilegio() {
+        return privilegio;
+    }
+    public String getNombre() {
+        return nombre;
     }
     public String getRol() {
         return rol;
